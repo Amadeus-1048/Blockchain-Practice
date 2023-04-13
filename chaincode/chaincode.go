91bfd0c209eef14bb0f5718569b33fcc587249da
amadeus <amadeus@1token.trade>
2023-04-13 18:48:48 +0800

    update: add api

--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -26,7 +26,7 @@ func (t *BlockChainRealEstate) Init(stub shim.ChaincodeStubInterface) pb.Respons
 		"4b227777d4dd",
 		"ef2d127de37b",
 	}
-	var userNames = [6]string{"管理员", "①号业主", "②号业主", "③号业主", "④号业主", "⑤号业主"}
+	var userNames = [6]string{"管理员", "①号", "②号", "③号", "④号", "⑤号"}
 	var balances = [6]float64{0, 5000000, 5000000, 5000000, 5000000, 5000000}
 	//初始化账号数据
 	for i, val := range accountIds {
@@ -49,30 +49,6 @@ func (t *BlockChainRealEstate) Invoke(stub shim.ChaincodeStubInterface) pb.Respo
 	switch funcName {
 	case "hello":
 		return api.Hello(stub, args)
-	case "queryAccountList":
-		return api.QueryAccountList(stub, args)
-	case "createRealEstate":
-		return api.CreateRealEstate(stub, args)
-	case "queryRealEstateList":
-		return api.QueryRealEstateList(stub, args)
-	case "createSelling":
-		return api.CreateSelling(stub, args)
-	case "createSellingByBuy":
-		return api.CreateSellingByBuy(stub, args)
-	case "querySellingList":
-		return api.QuerySellingList(stub, args)
-	case "querySellingListByBuyer":
-		return api.QuerySellingListByBuyer(stub, args)
-	case "updateSelling":
-		return api.UpdateSelling(stub, args)
-	case "createDonating":
-		return api.CreateDonating(stub, args)
-	case "queryDonatingList":
-		return api.QueryDonatingList(stub, args)
-	case "queryDonatingListByGrantee":
-		return api.QueryDonatingListByGrantee(stub, args)
-	case "updateDonating":
-		return api.UpdateDonating(stub, args)
 	default:
 		return shim.Error(fmt.Sprintf("没有该功能: %s", funcName))
 	}
