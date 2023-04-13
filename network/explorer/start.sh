e569d6436084d9801a88361a2e7cef2ec007684e
amadeus <amadeus@1token.trade>
2023-04-13 16:03:29 +0800

    feat: add chaincode

--- network/explorer/start.sh
+++ network/explorer/start.sh
@@ -4,7 +4,10 @@ priv_sk_path=$(ls ../crypto-config/peerOrganizations/taobao.com/users/Admin\@tao
 
 cp -rf ./connection-profile/network_temp.json ./connection-profile/network.json
 
-sed -i "s/priv_sk/$priv_sk_path/" ./connection-profile/network.json
+sed -i "" "s/priv_sk/$priv_sk_path/" ./connection-profile/network.json
 
 docker-compose down -v
 docker-compose up -d
+
+echo -e "\n\n\n"
+echo "已启动区块链浏览器，访问 http://localhost:8080/#/，用户名 admin，密码 123456"
