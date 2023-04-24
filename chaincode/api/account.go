54e4fca8c5aae42e89790d0613f9f7fc453e5942
amadeus <amadeus@1token.trade>
2023-04-24 14:58:40 +0800

    feat: add comment

--- chaincode/api/account.go
+++ chaincode/api/account.go
@@ -11,12 +11,16 @@ import (
 )
 
 // QueryAccountList 查询账户列表
+// stub：智能合约与区块链网络进行交互的接口，类型为 shim.ChaincodeStubInterface。
+// args：用于创建复合主键的键值列表，一个字符串数组。
 func QueryAccountList(stub shim.ChaincodeStubInterface, args []string) pb.Response {
 	var accountList []model.Account
+	// 根据 model.AccountKey 和 args 的前缀从账本中获取所有的符合条件的账户信息
 	results, err := utils.GetStateByPartialCompositeKeys(stub, model.AccountKey, args)
 	if err != nil {
 		return shim.Error(fmt.Sprintf("%s", err))
 	}
+	// 遍历账户信息，将其转换为 model.Account 对象添加到 accountList 列表中
 	for _, v := range results {
 		if v != nil {
 			var account model.Account
@@ -27,6 +31,7 @@ func QueryAccountList(stub shim.ChaincodeStubInterface, args []string) pb.Respon
 			accountList = append(accountList, account)
 		}
 	}
+	// 将 accountList 转换为字节数组，然后使用 shim.Success 方法将其作为查询结果返回给调用方
 	accountListByte, err := json.Marshal(accountList)
 	if err != nil {
 		return shim.Error(fmt.Sprintf("QueryAccountList-序列化出错: %s", err))
--- chaincode/chaincode.go
+++ chaincode/chaincode.go
@@ -11,11 +11,11 @@ import (
 	pb "github.com/hyperledger/fabric/protos/peer"
 )
 
-type BlockChainRealEstate struct {
+type BlockChainMedicalInfoManageSystem struct {
 }
 
-// Init 链码初始化
-func (t *BlockChainRealEstate) Init(stub shim.ChaincodeStubInterface) pb.Response {
+// Init 链码部署到链上并进行初始化时会执行该方法
+func (t *BlockChainMedicalInfoManageSystem) Init(stub shim.ChaincodeStubInterface) pb.Response { // stub 是智能合约中的一个对象，用于与区块链网络进行交互
 	fmt.Println("链码初始化")
 	//初始化默认数据
 	var accountIds = [6]string{
@@ -26,7 +26,7 @@ func (t *BlockChainRealEstate) Init(stub shim.ChaincodeStubInterface) pb.Respons
 		"4b227777d4dd",
 		"ef2d127de37b",
 	}
-	var userNames = [6]string{"管理员", "①号", "②号", "③号", "④号", "⑤号"}
+	var userNames = [6]string{"管理员", "①号业主", "②号业主", "③号业主", "④号业主", "⑤号业主"}
 	var balances = [6]float64{0, 5000000, 5000000, 5000000, 5000000, 5000000}
 	//初始化账号数据
 	for i, val := range accountIds {
@@ -44,11 +44,37 @@ func (t *BlockChainRealEstate) Init(stub shim.ChaincodeStubInterface) pb.Respons
 }
 
 // Invoke 实现Invoke接口调用智能合约
-func (t *BlockChainRealEstate) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
-	funcName, args := stub.GetFunctionAndParameters()
+func (t *BlockChainMedicalInfoManageSystem) Invoke(stub shim.ChaincodeStubInterface) pb.Response {
+	// 获取调用该智能合约函数时传入的参数和函数名
+	// 这些信息可以用于智能合约内部的逻辑处理，例如根据不同的函数名和参数执行不同的操作或者检查参数的有效性等
+	funcName, args := stub.GetFunctionAndParameters() // 返回两个值：funcName 表示函数名，args 表示函数参数列表，以字符串数组的形式返回
 	switch funcName {
 	case "hello":
 		return api.Hello(stub, args)
+	case "queryAccountList":
+		return api.QueryAccountList(stub, args)
+	case "createRealEstate":
+		return api.CreateRealEstate(stub, args)
+	case "queryRealEstateList":
+		return api.QueryRealEstateList(stub, args)
+	case "createSelling":
+		return api.CreateSelling(stub, args)
+	case "createSellingByBuy":
+		return api.CreateSellingByBuy(stub, args)
+	case "querySellingList":
+		return api.QuerySellingList(stub, args)
+	case "querySellingListByBuyer":
+		return api.QuerySellingListByBuyer(stub, args)
+	case "updateSelling":
+		return api.UpdateSelling(stub, args)
+	case "createDonating":
+		return api.CreateDonating(stub, args)
+	case "queryDonatingList":
+		return api.QueryDonatingList(stub, args)
+	case "queryDonatingListByGrantee":
+		return api.QueryDonatingListByGrantee(stub, args)
+	case "updateDonating":
+		return api.UpdateDonating(stub, args)
 	default:
 		return shim.Error(fmt.Sprintf("没有该功能: %s", funcName))
 	}
@@ -60,7 +86,7 @@ func main() {
 		panic(err)
 	}
 	time.Local = timeLocal
-	err = shim.Start(new(BlockChainRealEstate))
+	err = shim.Start(new(BlockChainMedicalInfoManageSystem)) // 启动智能合约
 	if err != nil {
 		fmt.Printf("Error starting Simple chaincode: %s", err)
 	}
